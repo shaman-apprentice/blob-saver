78bad008ebfd6be922a61a7ad219b09e24d12a8b
shaman-apprentice <torsten.knauf@gmx.de>
2023-11-26 20:27:20 +0100

    feat: Include types

--- src/main.mjs
+++ src/main.mjs
@@ -1,7 +1,7 @@
 /**
  * @param {string} fileName 
  * @param {Blob} data 
- * @param {number} [clearDelayInMs=60000] - This function creates an ObjectUrl via `URL.createObjectURL(data)`, which will be downloaded. After given delay it will be freed through a call to `URL.revokeObjectURL`. When you expect huge files, you might want to increase this delay, as the behavior of the download is not specified, when URL is freed, before download has finished.
+ * @param {number} [clearDelayInMs=60000] - (Default 60000) This function creates an ObjectUrl via `URL.createObjectURL(data)`, which will be downloaded. After given delay it will be freed through a call to `URL.revokeObjectURL`. When you expect huge files, you might want to increase this delay, as the behavior of the download is not specified, when URL is freed, before download has finished.
  */
 export function saveBlob(fileName, data, clearDelayInMs = 60000) {
   const aElement = document.createElement("a");
