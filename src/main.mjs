c217f5c6c409d91a22e494c03e5cdd2ab3524783
shaman-apprentice <Torsten.Knauf@gmx.de>
2024-01-20 13:07:53 +0100

    fix: Revoke internal object url

--- src/main.mjs
+++ src/main.mjs
@@ -4,13 +4,13 @@
  * @param {number} [clearDelayInMs=60000] - (Default 60000) This function creates an ObjectUrl via `URL.createObjectURL(data)`, which will be downloaded. After given delay it will be freed through a call to `URL.revokeObjectURL`. When you expect huge files, you might want to increase this delay, as the behavior of the download is not specified, when URL is freed, before download has finished.
  */
 export function saveBlob(fileName, data, clearDelayInMs = 60000) {
-  const aElement = document.createElement("a");
-  aElement.download = fileName;
-  aElement.href = URL.createObjectURL(data);
+  const a = document.createElement("a");
+  a.download = fileName;
+  a.href = URL.createObjectURL(data);
 
   setTimeout(() => {
     URL.revokeObjectURL(a.href);
   }, clearDelayInMs);
 
-  aElement.dispatchEvent(new MouseEvent("click"));
+  a.dispatchEvent(new MouseEvent("click"));
 }
